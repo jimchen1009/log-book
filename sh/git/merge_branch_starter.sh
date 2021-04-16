3c0b44023fb91e2e09907ba79a56e5edf8894f15
chenjingjun <chenjingjun@aobi.com>
2021-04-16 18:43:25 +0800

    BUG

--- sh/git/merge_branch_starter.sh
+++ sh/git/merge_branch_starter.sh
@@ -2,13 +2,13 @@
 #国服的工程
 local_projects=pjg-server-config,pjg-rpc,pjg-common,pjg-server,pjg-app-server,pjg-battle-server,pjg-http,routerserver,pjg-idip,pjg-picture
 #海外的工程
-bt_allprojects=pjg-server-config,pjg-rpc,pjg-common,pjg-server,pjg-app-server,pjg-battle-server,pjg-http,routerserver,pjg-pay,pjg-db-job
+bt_allprojects=pjg-server-config,pjg-rpc,pjg-common,pjg-server,pjg-app-server,pjg-battle-server,pjg-http,routerserver,pjg-pay,pjg-bgm,pjg-db-job
 
 from_branch=$1
 to_branch=$2
 
 allprojects=$local_projects
-if [ ["$3" == "bt"] ] 
+if [[ "$3" == bt ]] 
 then
 	allprojects=$bt_allprojects
 fi
